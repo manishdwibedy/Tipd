b519dc24a09d5a6b2e352e2e7d313dbfed8adc53
Manish <manish.dwibedy@gmail.com>
2015-10-09 16:16:37 -0700

    Trying with JBCS.

--- Tipd/ViewController.swift
+++ Tipd/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
 
     let tipPercents = [5,10,15,20,25]
@@ -31,6 +30,34 @@ class ViewController: UIViewController {
         totalAmount.text = totalAmountCalculated.description
     }
     func loadDatabase(){
+        let filemgr = NSFileManager.defaultManager()
+        let dirPaths =
+        NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
+            .UserDomainMask, true)
+        
+        let docsDir = dirPaths[0] as! String
+        
+        let databasePath = docsDir.stringByAppendingPathComponent(
+            "contacts.db")
+        
+        if !filemgr.fileExistsAtPath(databasePath as String) {
+            
+            let contactDB = FMDatabase(path: databasePath as String)
+            
+            if contactDB == nil {
+                println("Error: \(contactDB.lastErrorMessage())")
+            }
+            
+            if contactDB.open() {
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)"
+                if !contactDB.executeStatements(sql_stmt) {
+                    println("Error: \(contactDB.lastErrorMessage())")
+                }
+                contactDB.close()
+            } else {
+                println("Error: \(contactDB.lastErrorMessage())")
+            }
+        }
     }
     override func viewDidLoad() {
         
