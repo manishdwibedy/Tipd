0e9e71f5a227e3ca44202df5f2bde346fe263afd
Manish <manish.dwibedy@gmail.com>
2015-10-09 13:44:05 -0700

    First Commit

---
+++ Tipd/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tipd
+//
+//  Created by Student on 10/9/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
